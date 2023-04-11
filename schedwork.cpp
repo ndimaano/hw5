af4eb685f6aaeb4f53416529fa801cfeb7e09e34
Nicolas Dimaano <ndimaano@usc.edu>
2023-04-11 22:25:17 +0000

    Finished implementations of the recursive backtracking assignments

--- schedwork.cpp
+++ schedwork.cpp
@@ -18,9 +18,123 @@ using namespace std;
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
+/*
+Helper function that helps find the amount of days worked by
+any single worker within one solution 
+*/
+void workingDays (DailySchedule& sched, int counter, 
+int& nichi,int &row,int &col) { 
+  if(counter == static_cast<int>(sched.size())) {
+    return;
+  }
+  else {
+    nichi = nichi + count(sched[counter].begin(), sched[counter].end(), sched[row][col]);
+    workingDays(sched, counter + 1, nichi, row, col);
+  }
+}
+
+/*
+Helper function that helps find the amount of workers
+are working on this day and if we get to the end of the list 
+of workers and we have not already reached the required amount
+of workers we simply return false since a schedule that isn't
+full is not valid
+*/
+bool workersToday(DailySchedule& sched, int counter, int& shigotoWoShimasu,int &row,int &col) {
+	if(counter==col+1) {
+		return true;
+	}
+	else {
+		if(sched[row][counter] != INVALID_ID) {
+      if(sched[row][counter]==sched[row][col] && counter!=col) {
+        return false;
+      }
+      shigotoWoShimasu++;
+    }
+	}
+	return(workersToday(sched,counter+1,shigotoWoShimasu,row,col));
+}
+
+/*
+Helper function that checks whether or not the combination of numWorkers
+is a valid work schedule
+*/
+bool isValid (const AvailabilityMatrix& avail, const size_t dailyNeed,
+const size_t maxShifts, DailySchedule& sched, int &row,int &col) {
+  int nichi = 0;
+
+	// call helper function to find out how many days are worked for 
+	// the worker in the current position
+  workingDays(sched, 0, nichi, row, col);
+
+	// if the worker has worked more days than max shifts in this current
+	// iteration of schedule then return false so that it does not fill the
+	// rest of the schedule since we already know with how many we filled in that
+	// the worker is working too many days
+  if(nichi>static_cast<int>(maxShifts)) {
+          return false;
+  }
+
+
+	// here we check whether or not the day we are on has enough
+	// workers
+  int shigotoWoShimasu =0;
+
+	if(workersToday(sched, 0, shigotoWoShimasu, row, col)==false) {
+		return false;
+	}
+
+	// checks whether or not the day has enough workers 
+  if(col == static_cast<int>(sched[0].size()-1) && shigotoWoShimasu < static_cast<int>(dailyNeed)) {
+      return false;
+  }
+
+	//if the current worker we are on has already been set
+	// we will check whether or not this worker is worker is
+	// available to actually work that day
+  if(sched[row][col]!=INVALID_ID) {
+    if(!avail[row][sched[row][col]]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 // Add prototypes for any helper functions here
+bool helpMeMakeSchedule( const AvailabilityMatrix& avail, const size_t dailyNeed,
+const size_t maxShifts, DailySchedule& sched, int row, int col) {
+	// this if statetment handles the case where we are at the very
+	// last position in the schedule matrix and will return true since
+	// we were able to reach the end of the matrix without returning false
+	// in an earlier recursive call
+  if(row == static_cast<int>((sched.size()-1)) && col>static_cast<int>((sched[0].size()-1))) {
+    return true;
+  }
+	// this else if handles the case where we reach the end of one
+	// of the days amount of workers so it moves to the next day
+	// by increasing row and reseting col before calling the function
+  else if (row < static_cast<int>(sched.size()-1) && col>(static_cast<int>(sched[0].size()-1))) {
+    row++;
+    col=0;
+    return helpMeMakeSchedule(avail, dailyNeed, maxShifts, sched, row, col);
+  }
+	
+	// iterate through the set of workers
+  for(Worker_T i = 0; i < avail[0].size(); i++) {
+		//set a worker in a shift in the schedule
+    sched[row][col]=i;
+		// check if it is valid for the worker to be in that position
+    if(isValid(avail, dailyNeed, maxShifts, sched, row, col)) {
+			// move onto the next position in the schedule matrix
+      if(helpMeMakeSchedule(avail, dailyNeed, maxShifts, sched, row, col+1)) {
+        return true;
+      }
 
+    }
+  }
+  sched[row][col]=INVALID_ID;
+  return false;
+}
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -35,10 +149,13 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
 
-
-
-
+		//size schedule to fit the dimensions of n * d
+    sched.resize(avail.size());
+    for(int i = 0; i < static_cast<int>(avail.size()); i++) {
+      sched[i].resize(dailyNeed,INVALID_ID);
+    }
+		// make call to recursive helper function
+    return helpMeMakeSchedule(avail, dailyNeed,maxShifts,sched,0,0);
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,101 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+//The reason the runtime is bad is def because not considering cases where blanks are before the end
+// WORK MAINLY ON THIS ONE
+void helper(const std::string& in, const std::string& floating, const std::set<std::string>& dict, 
+std::string& temp, std::set<std::string>& combinations, int& usedFloats, std::string& angostura, int& KESHI) {
+	//start the function by finding the amount of blanks that have already been used
+	int usedBlanks = 0;
+	for(size_t i=0; i < temp.length(); i++) {
+		if(in[i] == '-') {
+			if(angostura.find(temp[i]) != std::string::npos) {
+				angostura.erase(angostura.find(temp[i]),1);
+				usedFloats++;
+			}
+			usedBlanks++;
+		}
+	}
+
+	// if there are more floats than there are spaces to fill then return
+	// because this should not be possible
+	if((floating.length() - usedFloats) > (in.length() - temp.length())) {
+		return;
+	}
+
+	// if the temp string is already the length of the word we are
+	// looking for then we can check if it is in the dictionary
+	// the other parts of the function already check if the
+	// floating letters are included
+	if(temp.length() == in.length()) { 
+			if(dict.find(temp)!=dict.end()) {
+				combinations.insert(temp);
+		}
+	}
+	// this handles the "green" letters from the wordle and 
+	// makes sure that we place the green letter into temp
+	// and later do not change it when we are trying to find 
+	// the other letters
+	else if(in[temp.length()]!='-' && !(temp.length()>=in.length())) {
+		// add the green letter to temp
+		temp = temp + in[temp.length()];
+		//reset counters
+		usedFloats=0;
+		angostura=floating;
+		// call helper function again
+		helper(in, floating, dict, temp, combinations, usedFloats, angostura, KESHI);
+		// remove whatever letter was added by the helper call to move
+		// onto the next letter in this letters place
+		if(temp.length()==1) {
+				temp = "";
+			}
+			else {
+				temp.pop_back();
+			}
+	}
+	// this handles the case where the amount of blanks left are
+	// equal to the amount of floating characters we have left which
+	// means that the remaining letters have to be the floating letters
+	else if(((floating.length() - usedFloats) == (in.length() - temp.length())) || (KESHI - usedBlanks <= static_cast<int>(angostura.length()))) {
+		for(int i =0; i < static_cast<int>(angostura.length()); i++ ) {
+			// angostura is holding the unused floating characters so
+			// we loop through the missing floating characters in the 
+			// remaining places
+			temp = temp + angostura[i];
+			// reset counters
+			usedFloats=0;
+			angostura=floating;
+			helper(in, floating, dict, temp, combinations, usedFloats, angostura, KESHI);
+			if(temp.length()==1) {
+				temp = "";
+			}
+			else {
+				temp.pop_back();
+			}
+		}
+	}
+	// this handles all other cases where the remaining letters do not
+	// equal floaters because the possible letter could be anything in the alphabet
+	else {
+		for(char i = 'a'; i <= 'z'; i++) { // for loop that iterates through the alphabet
+			temp = temp + i;
+			usedFloats=0;
+			angostura=floating;
+			helper(in, floating, dict, temp, combinations, usedFloats, angostura, KESHI);
+			if(temp.length()==1) {
+				temp = "";
+			}
+			else {
+				temp.pop_back();
+			}
+		}
+	}
+	//at the end of each helper call we have to set the amount of used floats to zero
+	//because when we backtrack to the letter before the letter we are currently on
+	//we may be removing letters that are floats so we have to count
+	//floats again for the next call to the function
+	usedFloats=0;
+}
 
 
 // Definition of primary wordle function
@@ -23,6 +118,60 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+		//create empty string to test words without affecting in or floating 
+		std::string temp = "";
+
+		//create a set of combinations to hold all words found in dictionary
+		std::set<std::string> combinations;
+
+		//create a counter to count the amount of floats used
+		int usedFloats = 0;
+
+		//create a string to be able to make changes to floating each
+		//iteration without altering floating
+		std::string floting = floating;
+
+		//get the number of missing letters that are unknown
+		int KESHI = 0;
+		for(size_t i =0; i < in.length(); i++) {
+			if(in[i]=='-') {
+				KESHI++;
+			}
+		}
+
+		//call the helper function that will find the dictionary words
+		helper(in, floating, dict, temp, combinations, usedFloats, floting, KESHI);
+		return combinations;
 }
 
 // Define any helper functions here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
